973fa2cd64838431c48b98f978953f3324cda3ac
Rem <kerem.bulut.006@gmail.com>
2026-08-23 00:49:45 +0200

    Updated version

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-fetch("https://api.github.com/repos/RemielMiku/rem.github.io/content/art")
+
+const gallery = document.getElementById("gallery");
+
+fetch("https://api.github.com/repos/RemielMiku/rem.github.io/contents/art")
     .then(response => response.json())
     .then(files => {
 
@@ -6,7 +9,7 @@ fetch("https://api.github.com/repos/RemielMiku/rem.github.io/content/art")
         const image = document.createElement("img");
         image.src = files[i].download_url;
         image.style.objectFit = "cover";
-        document.body.appendChild(image);
+        gallery.appendChild(image);
     }
 
 });
