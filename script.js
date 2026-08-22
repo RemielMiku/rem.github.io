89d02be74928226b7924a737f830a406f0590e30
Rem <kerem.bulut.006@gmail.com>
2026-08-23 01:16:36 +0200

    Images indexed through json file instead of github

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 
 const gallery = document.getElementById("gallery");
 
-fetch("https://api.github.com/repos/RemielMiku/rem.github.io/contents/art")
+fetch("art/imagedata.json")
     .then(response => response.json())
-    .then(files => {
-
-    for (let i = 0; i < files.length; i++) {
+    .then(art => {
+    
+    art.sort((a, b) => {
+        return new Number(b.index) - new Number(a.index);
+    });
+    
+    for (let i = 0; i < art.length; i++) {
         const image = document.createElement("img");
-        image.src = files[i].download_url;
-        image.style.objectFit = "cover";
+        
+        image.src = art[i].path;
+        image.alt = art[i].title;
+        
         gallery.appendChild(image);
     }
 
