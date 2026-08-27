71da1d94ae10355027e696f54dd0b8a93a25d519
Rem <kerem.bulut.006@gmail.com>
2026-08-27 14:10:44 +0200

    Minor optimization for site performance

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+    document.addEventListener("DOMContentLoaded", () => {
     const glassEffect = liquidGL({
         target: ".buttons, #gallery",
         snapshot: "body",
